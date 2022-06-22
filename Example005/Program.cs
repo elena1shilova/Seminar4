b12ebd403faad2040812328b1b4f605b53020118
elena1989 <elena.shilova89@gmail.com>
2022-06-22 13:35:11 +0300

    new5

---
+++ Example005/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+/*if (username == "Маша") //если введенное имя == Маша, но только если вводят также с большой буквы*/
+if (username.ToLower() == "маша") //если введенное имя == Маша с любым регистром
+
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else // иначе
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
