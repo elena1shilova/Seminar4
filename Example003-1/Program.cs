a35c9bddfbfc6287c511f41a0d29b22ce4da7ff3
elena1989 <elena.shilova89@gmail.com>
2022-06-22 13:22:33 +0300

    new3-1

---
+++ Example003-1/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,10); //вводим команду "новое случайное число в диапазоне от 1 до (10-1)" 
+Console.WriteLine(numberA);//выводим какое было случайное первое число, также и со вторым
+int numberB = new Random().Next(1,10); 
+Console.WriteLine(numberB);
+int result = numberA+numberB;
+Console.WriteLine(result);
