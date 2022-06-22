1c9691db72009b00df8e21d7863a55b7b48d6788
elena1989 <elena.shilova89@gmail.com>
2022-06-22 13:02:23 +0300

    new3

---
+++ Example003/Program.cs
@@ -0,0 +1,8 @@
+﻿/*int numberA = 3; // задаем первую переменную и указываем, что это целое число (int)
+int numberB = 5; // задаем вторую переменную и указываем, что это целое число (int)
+Console.WriteLine(numberA+numberB); //задаем формулу и выводим на экран результат*/
+// либо через промежут переменную
+int numberA = 3; 
+int numberB = 5; 
+int result = numberA+numberB;
+Console.WriteLine(result);
